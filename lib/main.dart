7b11b4d45947024895c6138565383ce48a761d6d
Mhmod <mahmoud97787@gmail.com>
2024-08-25 00:14:43 +0300

    App Root

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BusinessApp());
+}
+class BusinessApp extends StatelessWidget {
+  const BusinessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
