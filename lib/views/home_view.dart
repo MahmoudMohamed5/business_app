4bff6e8c615a62e9fa616e07a65172b9edd866bf
Mhmod <mahmoud97787@gmail.com>
2024-08-25 01:36:08 +0300

    add logo

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:business_app/utils/app_images.dart';
 import 'package:business_app/utils/constant.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,19 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: AppColors.backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 112,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 110,
+              backgroundImage: AssetImage(Assets.imagesLogo),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
